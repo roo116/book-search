c9d1cefa25e6481701c84c18b2891d65f1fde157
Chip Hill <hillkarl03@gmail.com>
2022-09-19 04:26:02 -0400

    created all server-side typedefs according to homework spec

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,30 @@ const typeDefs = gql`
     _id: ID
     username: String
     email: String
+    bookCount: Int
+    savedBooks: [Book]
   }
+
+  input savedBooksInput {
+  authors: Array
+  description: String
+  title: String
+  bookId: String
+  image: String
+  link: String
+  
+  }
+
+  type Book {
+  bookId: String
+  authors: Array
+  description: String
+  title: String
+  image: String
+  link: String
+  }
+
+  
 
   type Auth {
     token: ID
@@ -13,12 +36,13 @@ const typeDefs = gql`
   }
 
   type Query {
-    user(username: String!): User
+    me: User
   }
 
   type Mutation {
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-
+    savedBooks(input: [savedBooksInput!]!) User
   }
 `;
 
